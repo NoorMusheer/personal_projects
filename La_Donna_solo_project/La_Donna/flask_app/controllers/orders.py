5bdf78cbd65c73e26501fe851bdcb0fb28ba64ea
Noor Musheer <109195222+NoorMusheer@users.noreply.github.com>
2022-11-01 15:51:22 -0700

    Add files via upload

--- La_Donna_solo_project/La_Donna/flask_app/controllers/orders.py
+++ La_Donna_solo_project/La_Donna/flask_app/controllers/orders.py
@@ -5,8 +5,20 @@ from flask import render_template, redirect, request, session
 
 @app.route('/orders')
 def orders_page():
-    all_orders = order.Order.all_orders()
-    return render_template('orders_list.html', all_orders = all_orders)
+    active = {
+        "status":"active"
+    }
+    active_orders = order.Order.orders_by_status(active)
+    archived = {
+        "status":"archived"
+    }
+    archived_orders = order.Order.orders_by_status(archived)
+    return render_template('orders_list.html', active_orders = active_orders, archived_orders = archived_orders)
+
+@app.route('/archive_order/<int:id>')
+def update_to_archived(id):
+    order.Order.update_status_to_archive(id)
+    return redirect('/orders')
 
 @app.route('/new_order')
 def new_order():
@@ -19,6 +31,7 @@ def add_order_to_db():
         "dress_id":request.form['dress_id'],
         "bride_id":request.form['bride_id'],
         "notes":request.form['notes'],
+        "status":"active"
     }
     order.Order.add_order_to_db(data)
     return redirect('/orders')
--- La_Donna_solo_project/La_Donna/flask_app/models/order.py
+++ La_Donna_solo_project/La_Donna/flask_app/models/order.py
@@ -15,6 +15,20 @@ class Order:
         return connectToMySQL(cls.DB).query_db(query)
 
     @classmethod
+    def orders_by_status(cls, data):
+        query = "SELECT * FROM orders WHERE status = %(status)s ;"
+        return connectToMySQL(cls.DB).query_db(query, data)
+
+    @classmethod
+    def update_status_to_archive(cls, id):
+        data={
+            "id":id,
+            "status":"archived"
+        }
+        query = "UPDATE orders SET status = %(status)s WHERE id = %(id)s ;"
+        connectToMySQL(cls.DB).query_db(query, data)
+
+    @classmethod
     def add_order_to_db(cls, data):
-        query ="INSERT INTO orders (employee_id, dress_id, bride_id, notes, created_at, updated_at) VALUES (%(employee_id)s, %(dress_id)s, %(bride_id)s, %(notes)s, NOW(), NOW() );"
+        query ="INSERT INTO orders (employee_id, dress_id, bride_id, notes, status, created_at, updated_at) VALUES (%(employee_id)s, %(dress_id)s, %(bride_id)s, %(notes)s, %(status)s, NOW(), NOW() );"
         return connectToMySQL(cls.DB).query_db(query, data)
